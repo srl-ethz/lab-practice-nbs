c358afcb0a79285f93ca5ffecfe9866c2d11bd1f
Yasunori Toshimitsu <yasu313nori@gmail.com>
2023-03-09 21:38:05 +0100

    rendering works, recording video works!

--- simple_robot_envs/simple_robot.py
+++ simple_robot_envs/simple_robot.py
@@ -2,17 +2,22 @@ import numpy as np
 import gymnasium as gym
 from gymnasium.spaces import Box
 import mujoco
-import mujoco_viewer
 import os
+
+# Set up fake display; otherwise rendering will fail
+import os
+os.system("Xvfb :1 -screen 0 1024x768x24 &")
+os.environ['DISPLAY'] = ':1'
 
 class SimpleTorqueControlledRobot(gym.Env):
     """
     Simple gym implementation of a torque-controlled 2-joint robot, simulated in mujoco
-    rendering has not been tested yet
+    references:
     https://gymnasium.farama.org/tutorials/gymnasium_basics/environment_creation/#sphx-glr-tutorials-gymnasium-basics-environment-creation-py
+    https://pypi.org/project/mujoco/ (the Colab notebook linked here is a good sample for using mujoco in python)
     """
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
-    def __init__(self, render_mode=None):
+    metadata = {"render_modes": ["rgb_array"], "render_fps": 30}
+    def __init__(self, render_mode="rgb_array"):
 
         # set up mujoco simulation
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,21 +27,19 @@ class SimpleTorqueControlledRobot(gym.Env):
 
         # set up gym parameters
         self.action_space = Box(low=-1, high=1, shape=(self.num_joints,))
-        # joint range is -90 to 90 degrees, but mujoco's soft constraints may go beyond that,
-        # so set a larger range here to leave some slack
-        self.observation_space = Box(low=-np.pi, high=np.pi, shape=(self.num_joints*2,))
+        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(self.num_joints*2,))
 
+        # set up rendering
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
         if self.render_mode == "rgb_array":
-            self.viewer = mujoco_viewer.MujocoViewer(self.model, self.data, "offscreen")
-        elif self.render_mode == "human":
-            self.viewer = mujoco_viewer.MujocoViewer(self.model, self.data)
+            # self.ctx = mujoco.GLContext(600, 600)
+            self.renderer = mujoco.Renderer(self.model)
     
     def _get_obs(self):
         qpos = self.data.qpos
         qvel = self.data.qvel
-        return np.concatenate([qpos, qvel])
+        return np.concatenate([qpos, qvel], dtype=np.float32)
 
     def step(self, action):
         self.data.ctrl[:] = action
@@ -45,24 +48,23 @@ class SimpleTorqueControlledRobot(gym.Env):
         reward = 0
         done = False
 
-        if self.render_mode == "human":
-            self.viewer.render()
-
         return obs, reward, done, {}
     
     def close(self):
         pass
 
-    def reset(self, seed=None):
+    def reset(self, seed=None, options=None):
         # seed the random number generator
         super().reset(seed=seed)
         # TODO: set random initial state
         mujoco.mj_resetData(self.model, self.data)
+        mujoco.mj_forward(self.model, self.data)  # ensure rendering can be done
         obs = self._get_obs()
-        return obs
-    
+        return obs, {}
+
     def render(self):
         if self.render_mode != "rgb_array":
             return None
-        img = self.viewer.read_pixels()
-        return img
+        self.renderer.update_scene(self.data)
+        img = self.renderer.render()
+        return img.copy()
---
+++ utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
+
+def record_video_gym(env, length=100):
+    """
+    Record a video of the environment for a given number of steps and save it to a file.
+    """
+    vr = VideoRecorder(env, base_path="video", enabled=True)
+    obs = env.reset()
+    for _ in range(length):
+        action = np.ones(2)
+        vr.capture_frame()
+        obs, _, _, _ = env.step(action)
+    vr.close()
